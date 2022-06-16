57310ff10f5d4f1913aa8631fbb1e454fe10b27a
Elvis Oñate <62074670+erozedguy@users.noreply.github.com>
2022-06-15 19:45:40 -0500

    update AWS VPC terraform module (#1)
    Refactor terraform module

--- main.tf
+++ main.tf
@@ -1,5 +1,130 @@
-module "aws_vpc" {
-  source          = "./modules/aws_vpc"
-  networking      = var.networking
-  security_groups = var.security_groups
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = var.networking.cidr_block
+
+  tags = {
+    Name = var.networking.vpc_name
+  }
+}
+
+# PUBLIC SUBNETS
+resource "aws_subnet" "public_subnets" {
+  count                   = var.networking.public_subnets == null || var.networking.public_subnets == "" ? 0 : length(var.networking.public_subnets)
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = var.networking.public_subnets[count.index]
+  availability_zone       = var.networking.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet-${count.index}"
+  }
+}
+
+# PRIVATE SUBNETS
+resource "aws_subnet" "private_subnets" {
+  count                   = var.networking.private_subnets == null || var.networking.private_subnets == "" ? 0 : length(var.networking.private_subnets)
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = var.networking.private_subnets[count.index]
+  availability_zone       = var.networking.azs[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private_subnet-${count.index}"
+  }
+}
+
+# INTERNET GATEWAY
+resource "aws_internet_gateway" "i_gateway" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name = "i_gateway"
+  }
+}
+
+# EIPs
+resource "aws_eip" "elastic_ip" {
+  count      = var.networking.private_subnets == null || var.networking.nat_gateways == false ? 0 : length(var.networking.private_subnets)
+  vpc        = true
+  depends_on = [aws_internet_gateway.i_gateway]
+
+  tags = {
+    Name = "eip-${count.index}"
+  }
+}
+
+# NAT GATEWAYS
+resource "aws_nat_gateway" "nats" {
+  count             = var.networking.private_subnets == null || var.networking.nat_gateways == false ? 0 : length(var.networking.private_subnets)
+  subnet_id         = aws_subnet.public_subnets[count.index].id
+  connectivity_type = "public"
+  allocation_id     = aws_eip.elastic_ip[count.index].id
+  depends_on        = [aws_internet_gateway.i_gateway]
+}
+
+# PUBLIC ROUTE TABLE
+resource "aws_route_table" "public_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+}
+
+resource "aws_route" "public_routes" {
+  route_table_id         = aws_route_table.public_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.i_gateway.id
+}
+
+resource "aws_route_table_association" "assoc_public_routes" {
+  count          = length(var.networking.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_table.id
+}
+
+# PRIVATE ROUTE TABLES
+resource "aws_route_table" "private_tables" {
+  count  = length(var.networking.azs)
+  vpc_id = aws_vpc.custom_vpc.id
+}
+
+resource "aws_route" "private_routes" {
+  count                  = length(var.networking.private_subnets)
+  route_table_id         = aws_route_table.private_tables[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nats[count.index].id
+}
+
+resource "aws_route_table_association" "assoc_private_routes" {
+  count          = length(var.networking.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_tables[count.index].id
+}
+
+# SECURITY GROUPS
+resource "aws_security_group" "sec_groups" {
+  for_each    = { for sec in var.security_groups : sec.name => sec }
+  name        = each.value.name
+  description = each.value.description
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  dynamic "ingress" {
+    for_each = try(each.value.ingress, [])
+    content {
+      description      = ingress.value.description
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
+      protocol         = ingress.value.protocol
+      cidr_blocks      = ingress.value.cidr_blocks
+      ipv6_cidr_blocks = ingress.value.ipv6_cidr_blocks
+    }
+  }
+
+  dynamic "egress" {
+    for_each = try(each.value.egress, [])
+    content {
+      description      = egress.value.description
+      from_port        = egress.value.from_port
+      to_port          = egress.value.to_port
+      protocol         = egress.value.protocol
+      cidr_blocks      = egress.value.cidr_blocks
+      ipv6_cidr_blocks = egress.value.ipv6_cidr_blocks
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,21 @@
 variable "networking" {
   type = object({
-   cidr_block       = string
-   vpc_name         = string
-   azs              = list(string)
-   public_subnets   = list(string)
-   private_subnets  = list(string)
-   nat_gateways     = bool
+    cidr_block      = string
+    region          = string
+    vpc_name        = string
+    azs             = list(string)
+    public_subnets  = list(string)
+    private_subnets = list(string)
+    nat_gateways    = bool
   })
   default = {
-   cidr_block       = "10.0.0.0/16"
-   vpc_name         = "terraform-vpc"
-   azs              = ["us-east-1a", "us-east-1b"]
-   public_subnets   = ["10.0.1.0/24", "10.0.2.0/24"]
-   private_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
-   nat_gateways     = true
+    cidr_block      = "10.0.0.0/16"
+    region          = "us-east-1"
+    vpc_name        = "custom-vpc"
+    azs             = ["us-east-1a", "us-east-1b"]
+    public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+    nat_gateways    = true
   }
 }
 
@@ -21,26 +23,53 @@ variable "security_groups" {
   type = list(object({
     name        = string
     description = string
-    ingress = object({
+    ingress = list(object({
       description      = string
       protocol         = string
       from_port        = number
       to_port          = number
       cidr_blocks      = list(string)
       ipv6_cidr_blocks = list(string)
-    })
+    }))
+    egress = list(object({
+      description      = string
+      protocol         = string
+      from_port        = number
+      to_port          = number
+      cidr_blocks      = list(string)
+      ipv6_cidr_blocks = list(string)
+    }))
   }))
-
   default = [{
-    name        = "ssh"
-    description = "Port 22"
-    ingress = {
-      description      = "Allow SSH access"
-      protocol         = "tcp"
-      from_port        = 22
-      to_port          = 22
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = null
-    }
+    name        = "custom-security-group"
+    description = "Inbound & Outbound traffic for custom-security-group"
+    ingress = [
+      {
+        description      = "Allow HTTPS"
+        protocol         = "tcp"
+        from_port        = 443
+        to_port          = 443
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = null
+      },
+      {
+        description      = "Allow HTTP"
+        protocol         = "tcp"
+        from_port        = 80
+        to_port          = 80
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = null
+      },
+    ]
+    egress = [
+      {
+        description      = "Allow all outbound traffic"
+        protocol         = "-1"
+        from_port        = 0
+        to_port          = 0
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+      }
+    ]
   }]
 }
